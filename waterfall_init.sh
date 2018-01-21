54f26ee2d7cf31ba7a8f36ba71199a70fc9c02e4
OwnageTechGeek <OwnageTechGeek@users.noreply.github.com>
2018-01-21 17:03:52 -0500

    Update waterfall_init.sh

--- waterfall_init.sh
+++ waterfall_init.sh
@@ -5,6 +5,6 @@ chown -R waterfall.waterfall /$WATERFALL_HOME/
 
 
 cd /$WATERFALL_HOME/
-su - waterfall -c 'java -Xms128M -Xmx256M -XX:MaxPermSize=128M -jar Waterfall.jar'
+su - waterfall -c 'java -Xms64M -Xmx768M -XX:MaxPermSize=128M -DIReallyKnowWhatIAmDoingThisUpdate=true -jar Waterfall.jar'
 
 bash
