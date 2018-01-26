83a54b90c7325cf24d25114ed2c5d682d5b3feeb
OwnageTechGeek <OwnageTechGeek@users.noreply.github.com>
2018-01-26 12:47:04 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-chown -R waterfall /$PROCESSHOME/
+chown -R process /$PROCESSHOME/
 cd /$PROCESSHOME/
 su - process -c 'java -Xms64M -Xmx768M -XX:MaxPermSize=128M -DIReallyKnowWhatIAmDoingThisUpdate=true -jar Waterfall.jar'
 
