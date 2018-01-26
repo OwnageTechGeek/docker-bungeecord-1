cc364e404c46b9f616430900ca672f72015d089e
OwnageTechGeek <OwnageTechGeek@users.noreply.github.com>
2018-01-26 12:46:57 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 chown -R waterfall /$PROCESSHOME/
 cd /$PROCESSHOME/
-su - waterfall -c 'java -Xms64M -Xmx768M -XX:MaxPermSize=128M -DIReallyKnowWhatIAmDoingThisUpdate=true -jar Waterfall.jar'
+su - process -c 'java -Xms64M -Xmx768M -XX:MaxPermSize=128M -DIReallyKnowWhatIAmDoingThisUpdate=true -jar Waterfall.jar'
 
 bash
