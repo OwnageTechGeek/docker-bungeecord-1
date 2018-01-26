6a10db8fa14cdfc764283d93c94c8af4ffa7c6f6
OwnageTechGeek <OwnageTechGeek@users.noreply.github.com>
2018-01-26 12:44:32 -0500

    Update waterfall_init.sh

--- waterfall_init.sh
+++ waterfall_init.sh
@@ -1,10 +1,5 @@
-docker #!/bin/bash
-
-echo "eula=true" > /$WATERFALL_HOME/eula.txt
-chown -R waterfall.waterfall /$WATERFALL_HOME/
-
-
-cd /$WATERFALL_HOME/
+chown -R waterfall /$PROCESSHOME/
+cd /$PROCESSHOME/
 su - waterfall -c 'java -Xms64M -Xmx768M -XX:MaxPermSize=128M -DIReallyKnowWhatIAmDoingThisUpdate=true -jar Waterfall.jar'
 
 bash
